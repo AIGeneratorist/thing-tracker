71c2564d18671e73eaa626ad0dd2279dd19ae0c1
Alvin Hong <alvinhong2014@gmail.com>
2025-06-20 21:34:04 -0700

    Add thing fetching to home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,21 @@
-export default function Home() {
+async function getThings() {
+	const res = await fetch("http://localhost:3000/api/things");
+	if (!res.ok) {
+		throw new Error("Failed to fetch things");
+	}
+	return res.json();
+}
+
+export default async function Home() {
+	const things = await getThings();
 	return (
 		<>
 			<h1>Thing Tracker</h1>
+			<ul>
+				{things.map(thing => (
+					<li key={thing._id}>{thing.name}</li>
+				))}
+			</ul>
 		</>
 	);
 }
