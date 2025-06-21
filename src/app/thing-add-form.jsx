b6c37555ff0bfb422c39a97020d13ed6779b57b0
Alvin Hong <alvinhong2014@gmail.com>
2025-06-21 16:09:38 -0700

    Add more thing properties to client

--- src/app/thing-add-form.jsx
+++ src/app/thing-add-form.jsx
@@ -3,7 +3,35 @@
 import {useState} from "react";
 
 export default function ThingAddForm() {
-	const [name, setName] = useState("");
+	const [inputData, setInputData] = useState({
+		name: "",
+		type: "",
+		description: "",
+		favorited: false,
+		comments: ""
+	});
+	const [props, setProps] = useState([]);
+	const [nextPropId, setNextPropId] = useState(0);
+
+	async function handlePropAdd() {
+		setProps([
+			...props,
+			{id: nextPropId, name: "", value: ""}
+		]);
+		setNextPropId(nextPropId + 1);
+	}
+
+	async function handlePropChange(propId, name, value) {
+		setProps(
+			props.map(prop => {
+				return prop.id == propId ? {id: prop.id, name, value} : prop
+			})
+		);
+	}
+
+	async function handlePropRemove(propId) {
+		setProps(props.filter(prop => prop.id != propId));
+	}
 
 	async function handleSubmit(ev) {
 		ev.preventDefault();
@@ -13,15 +41,51 @@ export default function ThingAddForm() {
 			headers: {
 				"Content-Type": "application/json"
 			},
-			body: JSON.stringify({name})
+			body: JSON.stringify({
+				...inputData,
+				type: inputData.type || null,
+				description: inputData.description || null,
+				comments: inputData.comments || null,
+				props: props
+					.filter(prop => prop.name != "" && prop.value != "")
+					.reduce((acc, prop) => {
+						acc[prop.name] = prop.value;
+						return acc;
+					}, {})
+			})
 		})
 
 		if (!res.ok) {
 			throw new Error("Failed to add thing");
 		}
 
-		setName("");
+		setInputData({
+			name: "",
+			type: "",
+			description: "",
+			favorited: false,
+			comments: ""
+		})
+		setProps([]);
 	}
+
+	const propInputs = props.map(({id, name, value}) => (
+		<div key={id}>
+			<input
+				type="text"
+				name={`prop-${id}-name`}
+				value={name}
+				onChange={e => handlePropChange(id, e.target.value, value)}
+			/>
+			<input
+				type="text"
+				name={`prop-${id}-value`}
+				value={value}
+				onChange={e => handlePropChange(id, name, e.target.value)}
+			/>
+			<button onClick={() => handlePropRemove(id)}>&times;</button>
+		</div>
+	));
 
 	return (
 		<form onSubmit={handleSubmit}>
@@ -29,11 +93,52 @@ export default function ThingAddForm() {
 			<input
 				type="text"
 				name="name"
-				value={name}
+				value={inputData.name}
 				autoComplete="off"
 				id="input-name"
-				onChange={e => setName(e.target.value)}
+				onChange={ev => setInputData({...inputData, name: ev.target.value})}
 			/><br />
+
+			<label htmlFor="input-type">Type:</label>{" "}
+			<input
+				type="text"
+				name="type"
+				value={inputData.type}
+				id="input-type"
+				onChange={ev => setInputData({...inputData, type: ev.target.value})}
+			/><br />
+
+			<label htmlFor="input-description">Description:</label>{" "}
+			<input
+				type="text"
+				name="description"
+				value={inputData.description}
+				autoComplete="off"
+				id="input-description"
+				onChange={ev => setInputData({...inputData, description: ev.target.value})}
+			/><br />
+
+			<label htmlFor="chk-favorited">Favorite:</label>{" "}
+			<input
+				type="checkbox"
+				name="favorited"
+				checked={inputData.favorited}
+				id="chk-favorited"
+				onChange={ev => setInputData({...inputData, favorited: ev.target.checked})}
+			/><br />
+
+			<label htmlFor="ta-comments">Comments:</label>{" "}
+			<textarea
+				name="comments"
+				value={inputData.comments}
+				id="ta-comments"
+				onChange={ev => setInputData({...inputData, comments: ev.target.value})}
+			/><br />
+
+			Properties:
+			{propInputs}<br />
+			<button type="button" onClick={handlePropAdd}>Add Property</button><br />
+
 			<button type="submit">Add</button>
 		</form>
 	);
--- src/app/thing/[id]/page.jsx
+++ src/app/thing/[id]/page.jsx
@@ -12,8 +12,29 @@ export default async function ThingView({params}) {
 	return (
 		<>
 			<h1>View Thing</h1>
+
 			<p>{thing.name}</p>
+
 			<p>ID: {thing._id}</p>
+			<p>Type: {thing.type || <i>Unknown type</i>}</p>
+			<p>Description: {thing.description || <i>No description</i>}</p>
+			<p>Favorite: {thing.favorited ? "Yes" : "No"}</p>
+			<p>Comments: {thing.comments || <i>No comments</i>}</p>
+			<p>Properties:</p>
+			{
+				Object.keys(thing.props).length > 0 ?
+				(
+					<ul>
+						{Object.entries(thing.props).map(([prop, value]) => (
+							<li key={prop}>
+								{prop}: {value}
+							</li>
+						))}
+					</ul>
+				) : (
+					<i>No properties</i>
+				)
+			}
 		</>
 	);
 }
