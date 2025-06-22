3fe1ff78e54b9f2b3b3222d2e62e0a17bb3fc4b2
Alvin Hong <alvinhong2014@gmail.com>
2025-06-22 08:43:40 -0700

    Improve API error handling

--- src/app/api/things/[id]/route.js
+++ src/app/api/things/[id]/route.js
@@ -1,31 +1,65 @@
-import {ObjectId} from "mongodb";
+import {BSON, ObjectId} from "mongodb";
 import {thingsCollection} from "@/db/db.js";
 
 const allowedFields = ["name", "type", "description", "favorited", "comments", "props"];
 
 export const GET = async (req, {params}) => {
 	const {id} = await params;
-	const thing = await thingsCollection.findOne({_id: new ObjectId(id)});
-	return Response.json(thing);
+	try {
+		const thing = await thingsCollection.findOne({_id: new ObjectId(id)});
+		if (!thing) {
+			return Response.json({error: "Thing not found"}, {status: 404});
+		}
+		return Response.json(thing);
+	} catch (err) {
+		if (err instanceof BSON.BSONError) {
+			return Response.json({error: "Invalid ID"}, {status: 404});
+		}
+		return Response.json({error: `Server error: ${err}`}, {status: 500});
+	}
 };
 
 export const PATCH = async (req, {params}) => {
 	const {id} = await params;
-	const body = await req.json();
+	try {
+		const body = await req.json();
 
-	const newFields = allowedFields.reduce((acc, field) => {
-		if (body[field]) {
-			acc[field] = body[field];
+		const newFields = allowedFields.reduce((acc, field) => {
+			if (body[field]) {
+				acc[field] = body[field];
+			}
+			return acc;
+		}, {});
+		if (Object.keys(newFields).length == 0) {
+			return Response.json({error: "Invalid field name(s)"}, {status: 400});
 		}
-		return acc;
-	}, {});
 
-	await thingsCollection.updateOne({_id: new ObjectId(id)}, {$set: newFields});
-	return new Response();
+		const {matchedCount} = await thingsCollection.updateOne({_id: new ObjectId(id)}, {$set: newFields});
+		if (matchedCount == 0) {
+			return Response.json({error: "Thing not found"}, {status: 404});
+		}
+
+		return new Response();
+	} catch (err) {
+		if (err instanceof BSON.BSONError) {
+			return Response.json({error: "Invalid ID"}, {status: 404});
+		}
+		return Response.json({error: `Server error: ${err}`}, {status: 500});
+	}
 };
 
 export const DELETE = async (req, {params}) => {
 	const {id} = await params;
-	await thingsCollection.deleteOne({_id: new ObjectId(id)});
-	return new Response();
+	try {
+		const {deletedCount} = await thingsCollection.deleteOne({_id: new ObjectId(id)});
+		if (deletedCount == 0) {
+			return Response.json({error: "Thing not found"}, {status: 404});
+		}
+		return new Response();
+	} catch (err) {
+		if (err instanceof BSON.BSONError) {
+			return Response.json({error: "Invalid ID"}, {status: 404});
+		}
+		return Response.json({error: `Server error: ${err}`}, {status: 500});
+	}
 };
--- src/app/api/things/route.js
+++ src/app/api/things/route.js
@@ -1,25 +1,33 @@
 import {thingsCollection} from "@/db/db.js";
 
 export const GET = async () => {
-	const things = await thingsCollection.find().toArray();
-	return Response.json(things);
+	try {
+		const things = await thingsCollection.find().toArray();
+		return Response.json(things);
+	} catch (err) {
+		return Response.json({error: `Server error: ${err}`}, {status: 500});
+	}
 };
 
 export const POST = async req => {
-	const body = await req.json();
-	if (!body.name) {
-		return Response.json({error: "Missing name"}, {status: 400});
+	try {
+		const body = await req.json();
+		if (!body.name) {
+			return Response.json({error: "Missing name"}, {status: 400});
+		}
+
+		const thingData = {
+			name: body.name,
+			type: body.type || null,
+			description: body.description || null,
+			favorited: body.favorited || false,
+			comments: body.comments || null,
+			props: body.props || {}
+		};
+		const thing = await thingsCollection.insertOne(thingData);
+
+		return Response.json(thing);
+	} catch (err) {
+		return Response.json({error: `Server error: ${err}`}, {status: 500});
 	}
-
-	const thingData = {
-		name: body.name,
-		type: body.type || null,
-		description: body.description || null,
-		favorited: body.favorited || false,
-		comments: body.comments || null,
-		props: body.props || {}
-	};
-	const thing = await thingsCollection.insertOne(thingData);
-
-	return Response.json(thing);
 };
