6f688a6e8a4a312db8266b6a54216e6137619021
Alvin Hong <alvinhong2014@gmail.com>
2025-06-24 00:16:33 -0700

    Move query parameter parsing to util

--- src/app/api/things/route.js
+++ src/app/api/things/route.js
@@ -1,59 +1,15 @@
 import {thingSchema, thingsCollection} from "@/db/db.js";
-import {parseInputWithSchema} from "@/utils/utils.js";
+import {parseInputWithSchema, parseQueryParams} from "@/utils/utils.js";
 
 export const GET = async req => {
-	const rawPage = req.nextUrl.searchParams.get("page");
-	let page;
-	if (rawPage) {
-		page = parseInt(rawPage);
-		if (isNaN(page) || page < 1) {
-			return Response.json({error: "Invalid page"}, {status: 400});
-		}
-	} else {
-		page = 1;
-	}
-
-	const rawLimit = req.nextUrl.searchParams.get("limit");
-	let limit;
-	if (rawLimit) {
-		limit = parseInt(rawLimit);
-		if (isNaN(limit) || limit < 1) {
-			return Response.json({error: "Invalid limit"}, {status: 400});
-		}
-	} else {
-		limit = 25;
-	}
-
-	const rawSort = req.nextUrl.searchParams.get("sort");
-	let sortField;
-	let sortOrder;
-	if (rawSort) {
-		if (rawSort.endsWith("Desc")) {
-			sortField = rawSort.slice(0, -4);
-			sortOrder = "desc";
-		} else if (rawSort.endsWith("Asc")) {
-			sortField = rawSort.slice(0, -3);
-			sortOrder = "asc";
-		} else {
-			sortField = rawSort;
-			sortOrder = "asc";
-		}
-
-		if (!thingSchema[sortField] && sortField != "_id") {
-			return Response.json({error: "Invalid sort field"}, {status: 400});
-		}
-	} else {
-		sortField = "_id";
-		sortOrder = "desc";
+	const parseRes = parseQueryParams(req, thingSchema);
+	if (parseRes.error) {
+		return Response.json(parseRes, {status: 400});
 	}
 
 	try {
 		const [results, count] = await Promise.all([
-			thingsCollection.find({}, {
-				limit,
-				skip: (page - 1) * limit,
-				sort: [[sortField, sortOrder]]
-			}).toArray(),
+			thingsCollection.find({}, parseRes.data).toArray(),
 			thingsCollection.countDocuments()
 		]);
 		return Response.json({results, count});
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -17,3 +17,58 @@ export function parseInputWithSchema(input, schema, fillNulls = true) {
 	}
 	return acc;
 }
+
+export function parseQueryParams(req, schema) {
+	const rawPage = req.nextUrl.searchParams.get("page");
+	let page;
+	if (rawPage) {
+		page = parseInt(rawPage);
+		if (isNaN(page) || page < 1) {
+			return {error: "Invalid page"};
+		}
+	} else {
+		page = 1;
+	}
+
+	const rawLimit = req.nextUrl.searchParams.get("limit");
+	let limit;
+	if (rawLimit) {
+		limit = parseInt(rawLimit);
+		if (isNaN(limit) || limit < 1) {
+			return {error: "Invalid limit"};
+		}
+	} else {
+		limit = 25;
+	}
+
+	const rawSort = req.nextUrl.searchParams.get("sort");
+	let sortField;
+	let sortOrder;
+	if (rawSort) {
+		if (rawSort.endsWith("Desc")) {
+			sortField = rawSort.slice(0, -4);
+			sortOrder = "desc";
+		} else if (rawSort.endsWith("Asc")) {
+			sortField = rawSort.slice(0, -3);
+			sortOrder = "asc";
+		} else {
+			sortField = rawSort;
+			sortOrder = "asc";
+		}
+
+		if (!schema[sortField] && sortField != "_id") {
+			return {error: "Invalid sort field"};
+		}
+	} else {
+		sortField = "_id";
+		sortOrder = "desc";
+	}
+
+	return {
+		data: {
+			limit,
+			skip: (page - 1) * limit,
+			sort: [[sortField, sortOrder]]
+		}
+	};
+}
